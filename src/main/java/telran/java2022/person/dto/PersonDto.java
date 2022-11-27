4093cb5d15af505d25dde88c84482ad79ea4c99e
Alexander <Alexander@DESKTOP-R367M2D.mshome.net>
2022-11-27 12:54:49 +0200

    gogogo

---
+++ src/main/java/telran/java2022/person/dto/PersonDto.java
@@ -0,0 +1,16 @@
+package telran.java2022.person.dto;
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+
+@Getter 
+
+
+public class PersonDto {
+   Integer id;
+  String  name;
+  LocalDate  birthDate;
+   AddressDto address;
+       
+}
